f95000299475b3e4320d33098f0b3e37fef3f0da
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-24 23:10:14 -0500

    formating the legend text

--- graph.js
+++ graph.js
@@ -26,6 +26,7 @@ const legendGroup = svg.append('g')
 
 const legend = d3.legendColor()
     .shape('circle')
+    .shapePadding(10)
     .scale(color);
 
 //update function
@@ -35,6 +36,7 @@ function update(data) {
 
     //update and call legend
     legendGroup.call(legend);
+    legendGroup.selectAll('text').attr('fill', 'white');
 
     //join enhanced (pie) data to path elements
     const paths = graph.selectAll('path')
